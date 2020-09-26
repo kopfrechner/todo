4088b3f7d132e8bdd6e7abb7a13446e4c90d43b4
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 12:18:35 +0200

    add tenant service

---
+++ src/todo.core/ITenantService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Todo.Dal.Models;
+
+namespace Todo.Core
+{
+    public interface ITenantService
+    {
+        Task<IEnumerable<Tenant>> LoadAllTenantsAsync();
+    }
+}
---
+++ src/todo.core/TenantService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Todo.Dal;
+using Todo.Dal.Models;
+
+namespace Todo.Core
+{
+    public class TenantService : ITenantService
+    {
+        private readonly TodoContext _db;
+
+        public TenantService(TodoContext db)
+        {
+            _db = db;
+        }
+        
+        public async Task<IEnumerable<Tenant>> LoadAllTenantsAsync()
+        {
+            return await _db.Tenants.ToListAsync();
+        }
+    }
+}
