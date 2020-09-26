50ae848174e765faadf7402d9b0e1ad39cdbc7d4
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 12:18:48 +0200

    add service collection extension

---
+++ src/todo.core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Todo.Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddTodoServices(this IServiceCollection services)
+        {
+            services.AddTransient<ITenantService, TenantService>();
+            services.AddTransient<ITodoService, TodoService>();
+        }
+    }
+}
