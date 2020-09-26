61bcf18ab3f07617708f9427d941c344936d920e
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 10:56:14 +0200

    add design-time db context

---
+++ src/todo.dal/TodoContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace todo.dal
+{
+    public class TodoContextFactory : IDesignTimeDbContextFactory<TodoContext>
+    {
+        public TodoContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
+            optionsBuilder.UseSqlite("Data Source=:memory:");
+
+            return new TodoContext(optionsBuilder.Options);
+        }
+    }
+}
