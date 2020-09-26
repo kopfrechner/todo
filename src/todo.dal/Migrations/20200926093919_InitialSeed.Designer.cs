8f7dc8223da8c1a97795d182d7f3b9086cf430ee
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 11:41:12 +0200

    Add migration with data seed

--- src/todo.dal/Migrations/20200926093919_InitialSeed.Designer.cs
+++ src/todo.dal/Migrations/20200926093919_InitialSeed.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todo.Dal;
 
 namespace Todo.Dal.Migrations
 {
     [DbContext(typeof(TodoContext))]
-    partial class TodoContextModelSnapshot : ModelSnapshot
+    [Migration("20200926093919_InitialSeed")]
+    partial class InitialSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
