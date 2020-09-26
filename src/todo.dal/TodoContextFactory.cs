032ab1be3e40f4a19cb9f3584794a664982920b7
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 10:56:30 +0200

    add some comments

--- src/todo.dal/TodoContextFactory.cs
+++ src/todo.dal/TodoContextFactory.cs
@@ -3,13 +3,20 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace todo.dal
 {
+    /// <summary>
+    /// Create a default db context for design time
+    /// </summary>
+    /// <remarks>
+    /// With this class we can cerate db-migrations, without an actual database
+    /// dotnet ef migrations add <MIGRATION-NAME>
+    /// </remarks>
     public class TodoContextFactory : IDesignTimeDbContextFactory<TodoContext>
     {
         public TodoContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
-            optionsBuilder.UseSqlite("Data Source=:memory:");
-
+            optionsBuilder.UseSqlite("Data Source=:memory:"); // simply use an in-memory database
+            
             return new TodoContext(optionsBuilder.Options);
         }
     }
