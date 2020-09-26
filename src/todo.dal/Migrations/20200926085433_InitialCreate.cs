ca7b470e320e6131bc53cdc7facd00f41c8f27ef
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 10:57:37 +0200

    add initial migration

---
+++ src/todo.dal/Migrations/20200926085433_InitialCreate.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace todo.dal.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoLists",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TenantId = table.Column<Guid>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoLists_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalSchema: "dbo",
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TenantId = table.Column<Guid>(nullable: true),
+                    DoWhat = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Done = table.Column<bool>(nullable: false),
+                    TotoListId = table.Column<Guid>(nullable: true),
+                    TodoListId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoItems_Tenants_TenantId",
+                        column: x => x.TenantId,
+                        principalSchema: "dbo",
+                        principalTable: "Tenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_TodoItems_TodoLists_TotoListId",
+                        column: x => x.TotoListId,
+                        principalSchema: "dbo",
+                        principalTable: "TodoLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_TenantId",
+                schema: "dbo",
+                table: "TodoItems",
+                column: "TenantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItems_TotoListId",
+                schema: "dbo",
+                table: "TodoItems",
+                column: "TotoListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoLists_TenantId",
+                schema: "dbo",
+                table: "TodoLists",
+                column: "TenantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItems",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "TodoLists",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Tenants",
+                schema: "dbo");
+        }
+    }
+}
