0ed4de4c87efeb7a88de61c2148fa79ede6a6472
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 11:46:46 +0200

    add service

---
+++ src/todo.core/TodoService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Todo.Dal;
+using Todo.Dal.Models;
+
+namespace todo.core
+{
+    public class TodoService
+    {
+        private readonly TodoContext _db;
+
+        public TodoService(TodoContext db)
+        {
+            _db = db;
+        }
+        
+        public async Task<IEnumerable<TotoList>> LoadTodoListsAsync(Guid tenantId)
+        {
+            return await _db.TodoLists.Where(x => x.TenantId == tenantId).ToListAsync();
+        }
+        
+        public async Task<IEnumerable<TodoItem>> LoadTodoItemsAsync(Guid tenantId, Guid todoListId)
+        {
+            return await _db.TodoItems.Where(x => x.TenantId == tenantId && x.TodoListId == todoListId).ToListAsync();
+        }
+    }
+}
