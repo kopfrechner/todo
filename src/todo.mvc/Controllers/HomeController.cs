f16c6381494dc54bafc930459e7b2c2d1ff55724
Christoph Kopf <christoph.kopf@adliance.net>
2020-09-26 12:19:59 +0200

    use services in controller

--- src/todo.mvc/Controllers/HomeController.cs
+++ src/todo.mvc/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Todo.Core;
 using Todo.Mvc.Models;
 
 namespace Todo.Mvc.Controllers
@@ -12,14 +13,25 @@ namespace Todo.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITenantService _tenantService;
+        private readonly ITodoService _todoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            ITenantService tenantService,
+            ITodoService todoService)
         {
             _logger = logger;
+            _tenantService = tenantService;
+            _todoService = todoService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var tenants = await _tenantService.LoadAllTenantsAsync();
+            var aTenant = tenants.First();
+            
+            var todoLists= await _todoService.LoadTodoListsAsync(aTenant.Id);
+
             return View();
         }
 
---
+++ src/todo.mvc/Models/Home/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Todo.Mvc.Models.Home
+{
+    public class IndexViewModel
+    {
+        public string TenantName { get; set; }
+        public IEnumerable<string> TodoListNames { get; set; }
+    }
+}
